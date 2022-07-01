0b87c5db425a50a7d66ebecd6600da9282046499
Suman Thapa <kajithapasuman@gmail.com>
2022-07-01 16:12:28 +1000

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Kaji Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying" >Play</button>
+  <app-block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <app-results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppBlock from './components/AppBlock'
+import AppResults from './components/AppResults'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: {AppBlock, AppResults},
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false,
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -22,5 +41,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
